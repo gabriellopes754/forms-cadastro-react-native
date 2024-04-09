50ab7f2dca4441d89e4954dbb7a395f86b8488d9
gabriellopes754 <gabriellopes15942@outlook.com>
2024-04-08 23:12:24 -0300

    finalizado

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput,Alert} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput,Alert, ScrollView} from 'react-native';
 import Checkbox from "expo-checkbox";
 
 export default function App() {
@@ -11,19 +11,27 @@ export default function App() {
   const [celular, setCelular] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [endereco, setEndereco] = React.useState('');
-  const [curso, setCurso] = React.useState('');
-  const [checkbox, setcheck] = React.useState('');
+  const [administracao, setadministracao] = React.useState(false);
+  const [desenvolvimento, setdesenvolvimento] = React.useState(false);
+  const [enfermagem, setenfermagem] = React.useState(false);
 
+  const validacao = () => {
+    if ((rg === '' || nome === '' || celular === '' || email === '' || endereco === '')) {
+      Alert.alert('Preencha todos os campos obrigatórios');
+    }else if (administracao == false && desenvolvimento == false && enfermagem == false){
+      Alert.alert('Preenchimeto do curso obrigatório!')
+    } else if (parseInt(idade) < 18) {
+      Alert.alert('Usuário menor de idade', 'não é possível efetuar cadastro');
+    } else {
+        Alert.alert('Cadastro efetuado com sucesso');
+    }};
 
-
-const validacao = () =>{
-    if ((idade) < '18') {
-      Alert.alert('Usuário menor de idade, não é possivel efetuar cadastro');
-    }
 
   return (
+    
     <View style={styles.container}>
-      <Text>Formulário!</Text>
+    <ScrollView>
+      <Text style={styles.field}>Formulário de Cadastro!</Text>
       <StatusBar style="auto" />
 
       <Text style={styles.field}>RG:</Text>
@@ -32,40 +40,49 @@ const validacao = () =>{
       <Text style={styles.field}>NOME:</Text>
       <TextInput onChangeText = {setNome} value={nome} style={styles.textinho} />
 
-      <Text style={styles.field}>idade</Text>
+      <Text style={styles.field}>Idade</Text>
       <TextInput keyboardType='numeric' onChangeText = {setIdade} value={idade} style={styles.textinho} />
 
-       <Text style={styles.field}>celular:</Text>
+       <Text style={styles.field}>Celular:</Text>
       <TextInput keyboardType='numeric' onChangeText = {setCelular} value={celular} style={styles.textinho} />
 
       <Text style={styles.field}>Endereço:</Text>
       <TextInput  onChangeText = {setEndereco} value={endereco} style={styles.textinho} /> 
 
       <Text style={styles.field}>E-mail:</Text>
-      <TextInput  onChangeText = {setEmail} value={email} style={styles.textinho} /> 
+      <TextInput  onChangeText = {setEmail} value={email} style={styles.textinho} />
+      
+      <Text style={styles.textocheck}>Selecione o curso desejado:</Text>
+          <View style={styles.checkboxContainer}>
+            <Checkbox value={administracao} onValueChange={setadministracao} color={administracao ? 'red' : undefined}style={{margin: 8}}/>
+            <Text>Administração</Text>
+          </View>
+          <View style={styles.checkboxContainer}>
+            <Checkbox value={desenvolvimento} onValueChange={setdesenvolvimento} color={desenvolvimento ? 'red' : undefined}style={{margin: 8}}/>
+            <Text>Desenvolvimento de Sistemas </Text>
+          </View>
+          <View style={styles.checkboxContainer}>
+            <Checkbox value={enfermagem} onValueChange={setenfermagem} color={enfermagem ? 'red' : undefined} style={{margin: 8}}/>
+            <Text>Enfermagem </Text>
+          </View>
 
-      <Text style={styles.field}>Curso:</Text>
-      <TextInput  onChangeText = {setCurso} value={curso} style={styles.textinho} /> 
-{/* 
-      <Text style={styles.textinho}>Selecione o curso desejado:</Text>
-          <Text>Administração</Text>
-          <Checkbox value={checkbox} onValueChange={setcheck} />
-          <Text>Desenvolvimento de Sistemas</Text>
-          <Checkbox value={checkbox} onValueChange={setcheck} />
-          <Text>Enfermagem</Text>
-          <Checkbox value={checkbox} onValueChange={setcheck} /> */}
+
+
 
       <Button
         title="Validar"
         onPress= {validacao}
       />
+  </ScrollView>
     </View>
+    
   );  
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop:30,
     backgroundColor: 'rgb(105,105,105)',
     alignItems: 'center',
     justifyContent: 'center',
@@ -78,18 +95,19 @@ const styles = StyleSheet.create({
 
   textinho: {
     backgroundColor: 'rgb(54,54,54)',
-    width: '80%',
-    borderRadius:10,
-    padding:10,
-    marginTop:10,
+    width: '90%',
+    height: 30,
+    borderRadius:5,
+    padding:0,
+    marginTop:5,
   },
 
-  tex2: {
+  textinho2: {
       height: 40,
       borderColor: 'gray',
       borderWidth: 1,
       marginTop: 10,
-      paddingHorizontal: 10,
+      paddingHorizontal:0,
       width: '80%',
   },
 
@@ -97,8 +115,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
   },
-  checkbox: {
-    alignSelf: 'center',
+  textocheck: {
+    fontSize: 18,
+    marginTop: 5,
+    color: "#000",
+    margin: 8
   },
 
-  });}
+  });
